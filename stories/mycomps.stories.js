c022dc2858aa11dbb5699710d7d0bcc536b5c9f1
ariellecastro <caastrox@gmail.com>
2021-02-25 20:45:47 -0800

    Arielle added tab + button

--- stories/mycomps.stories.js
+++ stories/mycomps.stories.js
@@ -1,7 +1,10 @@
 import {ButtonUI} from '../accomps/button';
+import {TabUI} from '../accomps/tab';
 
 export default{
     title: "My Button"
 }
 
 export const MyButtonUI = () => ButtonUI;
+export const MyTabUI = () => TabUI;
+
--- stories/workflow.js
+++ stories/workflow.js
@@ -1,1 +1,4 @@
 document.querySelector(".mybutton").innerHTML += ButtonUI();
+document.querySelector(".mybutton").innerHTML += ButtonUI("TIP 2", "https://www.flaticon.com/svg/vstatic/svg/1689/1689198.svg?token=exp=1614314409~hmac=d0c5a88c93002f7b6dae6e274a512912");
+
+document.querySelector(".tab").innerHTML += TabUI();
