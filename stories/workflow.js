47b36fc8fc2425d9d2b66b5bf7402a4b8da369dd
AliciaYien <ayien1@my.bcit.ca>
2021-03-01 17:50:00 -0800

    commit

--- stories/workflow.js
+++ stories/workflow.js
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
 document.querySelector(".BackNextButton").innerHTML += BackNextButtonUI("Back");
 document.querySelector(".BackNextButton").innerHTML += BackNextButtonUI("Next");
 
 document.querySelector(".textbubble").innerHTML += TextBubbleUI("More than 30% of the landfill is taken up by compostable organics and food scraps");
 document.querySelector(".textbubble").innerHTML += TextBubbleUI();
-=======
-<<<<<<< HEAD
+
 document.querySelector(".mybutton").innerHTML += ButtonUI();
 document.querySelector(".mybutton").innerHTML += ButtonUI("TIP 2", "https://www.flaticon.com/svg/vstatic/svg/1689/1689198.svg?token=exp=1614314409~hmac=d0c5a88c93002f7b6dae6e274a512912");
 
 document.querySelector(".tab").innerHTML += TabUI();
-=======
->>>>>>> 5aac6633c5eef674adc6691e4a8ceb2ada681282
 document.querySelector(".Header"). innerHTML += HeaderUI();
-
->>>>>>> f12761d44a20831f0a36d325715f5b81d43ae993
 
 document.querySelector(".SubHeader"). innerHTML += SubHeaderUI();
 
