cd294bc43e58e3f3c07db8354e8d8609d8ffd198
JessicaW0 <jwu326@my.bcit.ca>
2021-03-02 20:10:40 -0800

    Logo and start button

--- stories/JessMyComps.stories.js
+++ stories/JessMyComps.stories.js
@@ -1,5 +1,3 @@
-import {Header} from '../comps/header';
-import {SubHeader} from '../comps/subheader';
 
 import {BackNextButton} from '../comps/BackNextButton';
 import {TextBubble} from '../comps/TextBubble';
@@ -10,18 +8,25 @@ import {ButtonUI} from '../accomps/button';
 import {TabUI} from '../accomps/tab';
 import {TrashCan} from '../accomps/trash-can';
 
-
+//Jessica
+import {Header} from '../comps/header';
+import {SubHeader} from '../comps/subheader';
+import {StartButton} from '../comps/StartButton';
+import {LogoUI} from '../comps/Logo';
 
 export default {
     title: "My Components"
 }
 
+//Jessica
 export const MyHeader = () => Header;
 export const MySubHeader = () => SubHeader;
-
+export const StartButtonUI = () => StartButton;
+export const MyLogoUI = () => LogoUI;
 
 export const MyBackNextButton = () => BackNextButton;
 export const MyTextBubble = () => TextBubble;
+
 
 //Arielle
 export const MyButtonUI = () => ButtonUI;
