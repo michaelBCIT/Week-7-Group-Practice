871603e3589395832bc8480c0447c4b224e7b74f
JessicaW0 <jwu326@my.bcit.ca>
2021-03-06 15:20:49 -0800

    commit

---
+++ pages/page5/page5-2.js
@@ -0,0 +1,3 @@
+document.querySelector(".TextBubble-Page5-2").innerHTML += textBubbleLightUI("RESOURCE COST", "204px", "#FBAD81");
+document.querySelector(".BackButton-Page5-2").innerHTML += BackNextButtonUI("Back");
+document.querySelector(".NextButton-Page5-2").innerHTML += BackNextButtonUI("Next");
