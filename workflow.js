26e47a2f09527414a74e8be9452a8a84a82c339e
ariellecastro <caastrox@gmail.com>
2021-03-06 14:24:39 -0800

    Changed some workflow

--- workflow.js
+++ workflow.js
@@ -35,10 +35,14 @@ document.querySelector(".textbubblepage3-2").innerHTML += textBubbleLightUI("RES
 document.querySelector(".button-p3-1").innerHTML += BackNextButtonUI("Back");
 document.querySelector(".button-p3-2").innerHTML += BackNextButtonUI("Next");
 
+//page4
 
 document.querySelector(".headerpage4").innerHTML += HeaderUI("THE COST OF FOOD WASTE");
 document.querySelector(".textBubblePage3-1").innerHTML += textBubbleLightUI("Food waste negatively contributes to resource costs.", "245px", "#F5D9B7");
 document.querySelector(".textBubblePage3-2").innerHTML += textBubbleLightUI("RESOURCE COST", "204px", "#FBAD81");
+
+document.querySelector(".button-p4-1").innerHTML += BackNextButtonUI("Back");
+document.querySelector(".button-p4-2").innerHTML += BackNextButtonUI("Next");
 
 
 // page 9
