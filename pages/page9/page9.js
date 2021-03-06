e05083525062151e609a21965553bdfa3e957d9b
AliciaYien <ayien1@my.bcit.ca>
2021-03-06 15:20:50 -0800

    pull

--- pages/page9/page9.js
+++ pages/page9/page9.js
@@ -2,3 +2,5 @@ document.querySelector(".header-p9").innerHTML += HeaderUI("WHAT CAN CLEAN TABLE
 document.querySelector(".touchofabutton").innerHTML += textBubbleLightUI("With a touch of a button, users can help list their groceries, and track their expiration dates.");
 
 document.querySelector(".createvisual").innerHTML += textBubbleLightUI("Create a visual grocery list, that shows the use when their products expire and also shows what items they have inside their pantry or fridge.");
+
+document.querySelector(".makerecipiesoutof").innerHTML += textBubbleLightUI("Make recipies out of the igredients that are about to expire.");
