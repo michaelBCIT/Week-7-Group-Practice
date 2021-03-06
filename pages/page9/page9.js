65bd37a146c9c77a392fc9fef87f220ac9a22df1
AliciaYien <ayien1@my.bcit.ca>
2021-03-06 15:17:06 -0800

    .

--- pages/page9/page9.js
+++ pages/page9/page9.js
@@ -1,2 +1,4 @@
 document.querySelector(".header-p9").innerHTML += HeaderUI("WHAT CAN CLEAN TABLE DO FOR YOU?");
-document.querySelector(".touchofabutton").innerHTML += textBubbleLightUI("With a touch of a button, users can help list their groceries, and track their expiration dates");
+document.querySelector(".touchofabutton").innerHTML += textBubbleLightUI("With a touch of a button, users can help list their groceries, and track their expiration dates.");
+
+document.querySelector(".createvisual").innerHTML += textBubbleLightUI("Create a visual grocery list, that shows the use when their products expire and also shows what items they have inside their pantry or fridge.");
