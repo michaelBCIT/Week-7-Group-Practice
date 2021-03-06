9ce1f9bf7060050d7306def57b2d24101bac2a3d
ariellecastro <caastrox@gmail.com>
2021-03-06 11:50:09 -0800

    Repositioned the 'start' button on page 1

--- workflow.js
+++ workflow.js
@@ -1,5 +1,5 @@
-document.querySelector(".button-p2-1").innerHTML += BackNextButtonUI("Back");
-document.querySelector(".button-p2-2").innerHTML += BackNextButtonUI("Next");
+//document.querySelector(".button-p2-1").innerHTML += BackNextButtonUI("Back");
+//document.querySelector(".button-p2-2").innerHTML += BackNextButtonUI("Next");
 document.querySelector(".startbutton").innerHTML += StartButtonUI("Next");
 
 
@@ -70,8 +70,8 @@ document.querySelector(".textBubblePage3-2").innerHTML += textBubbleLightUI("RES
 
 
 //page 3
-document.querySelector(".HeaderPage3").innerHTML += HeaderUI("THE COST OF FOOD WASTE");
-document.querySelector(".textBubblePage3-1").innerHTML += textBubbleLightUI();
+//document.querySelector(".HeaderPage3").innerHTML += HeaderUI("THE COST OF FOOD WASTE");
+//document.querySelector(".textBubblePage3-1").innerHTML += textBubbleLightUI();
 
 function NextPage(page){
   document.querySelector("#show").style.display = "none";
