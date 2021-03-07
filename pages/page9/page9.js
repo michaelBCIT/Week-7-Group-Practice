564f639a2bfe04fe630303700085e448cb93631e
AliciaYien <ayien1@my.bcit.ca>
2021-03-06 16:22:58 -0800

    page9

--- pages/page9/page9.js
+++ pages/page9/page9.js
@@ -4,3 +4,11 @@ document.querySelector(".touchofabutton").innerHTML += textBubbleLightUI("With a
 document.querySelector(".createvisual").innerHTML += textBubbleLightUI("Create a visual grocery list, that shows the use when their products expire and also shows what items they have inside their pantry or fridge.");
 
 document.querySelector(".makerecipiesoutof").innerHTML += textBubbleLightUI("Make recipies out of the igredients that are about to expire.");
+
+document.querySelector(".cleantablewillgiveusers").innerHTML += textBubbleLightUI("Clean Table will give users confidence to cook more from home instead of using food delivery. As well as spread awareness about healthy eating while reducing garbage production.");
+
+document.querySelector(".cleantablewillgiveusers").innerHTML += BackNextButtonUI("Back");
+
+document.querySelector(".footer").innerHTML += MyFooterUI();
+
+document.querySelector(".subheader").innerHTML += SubHeaderUI("environment");
