745528e935b57f2a091392fdfb032c997e1986d2
ariellecastro <caastrox@gmail.com>
2021-03-06 18:24:38 -0800

    Made page 8

---
+++ pages/page8/page8.js
@@ -0,0 +1,12 @@
+
+document.querySelector(".headerpage8").innerHTML += HeaderUI("HOW YOU CAN REDUCE FOOD WASTE");
+
+document.querySelector(".tip1").innerHTML += ButtonUI("TIP 1");
+
+document.querySelector(".tip1").innerHTML += ButtonUI("TIP 2");
+
+document.querySelector(".tip1").innerHTML += ButtonUI("TIP 3");
+
+document.querySelector(".BackButton-Page8-1").innerHTML += BackNextButtonUI("Back");
+
+document.querySelector(".BackButton-Page8-2").innerHTML += BackNextButtonUI("Next");
