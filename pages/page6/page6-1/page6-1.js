1a622c16b4fcba0ab04d9b8e0d1c30f2d53a5fe1
AliciaYien <ayien1@my.bcit.ca>
2021-03-06 19:45:28 -0800

    .

--- pages/page6/page6-1/page6-1.js
+++ pages/page6/page6-1/page6-1.js
@@ -2,21 +2,13 @@ document.querySelector(".headerP6-1").innerHTML += HeaderUI("WHY DOES ALL THIS H
 
 document.querySelector(".withallthereasons").innerHTML += textBubbleLightUI("With all the reasons why food waste is bad for us and our world, why does it happen at all?");
 
-
-document.querySelector(".bgbubble").innerHTML += textBubbleUI();
-
-document.querySelector(".bgbubble").innerHTML += TextBubbleUI();
-
+document.querySelector(".bgbubble").innerHTML += textBubbleLightUI("none");
 
 document.querySelector(".familiesandind").innerHTML += textBubbleLightUI("Families and individuals that stock too much food that spoils before it can be sold or eaten.");
 
 document.querySelector(".toofindout").innerHTML += textBubbleLightUI("To find out how much good food Canadians throw away");
 
 document.querySelector(".back").innerHTML += BackNextButtonUI("Back");
-
-document.querySelector(".back").innerHTML += BackNextButtonUI("Next");
-
 document.querySelector(".next").innerHTML += BackNextButtonUI("Next");
-
 
 //michael was here 321
