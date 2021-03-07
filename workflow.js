2f7dc0b0068caa15f280b0a5272d746f69b28c27
ariellecastro <caastrox@gmail.com>
2021-03-07 12:30:03 -0800

    commit

--- workflow.js
+++ workflow.js
@@ -72,7 +72,7 @@ document.querySelector(".textbubble7-1").innerHTML += textBubbleLightUI("That's 
 
 //page 8 
 document.querySelector(".headerpage8").innerHTML += HeaderUI("HOW YOU CAN REDUCE FOOD WASTE");
-document.querySelector(".tip1").innerHTML += MyButtonUI("TIP 1");
+document.querySelector(".tip1").innerHTML += MyButtonUI("TIP 1", "./stories/info-pics/");
 document.querySelector(".tip1").innerHTML += MyButtonUI("TIP 2");
 document.querySelector(".tip1").innerHTML += MyButtonUI("TIP 3");
 document.querySelector(".BackButton-Page8-1").innerHTML += BackNextButtonUI("Back");
