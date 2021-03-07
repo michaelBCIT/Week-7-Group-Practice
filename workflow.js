69e28207f882b659a88ad3534a201c23a76950d3
ariellecastro <caastrox@gmail.com>
2021-03-06 21:23:16 -0800

    Added 6-1 to html + js

--- workflow.js
+++ workflow.js
@@ -46,6 +46,15 @@ document.querySelector(".SubHeaderPage5-2").innerHTML += SubHeaderUI("69px", "34
 document.querySelector(".BackButton-Page5-2").innerHTML += BackNextButtonUI("Back");
 document.querySelector(".NextButton-Page5-2").innerHTML += BackNextButtonUI("Next");
 
+//page 6-2
+document.querySelector(".headerP6-1").innerHTML += HeaderUI("WHY DOES ALL THIS HAPPEN?");
+document.querySelector(".withallthereasons").innerHTML += textBubbleLightUI("With all the reasons why food waste is bad for us and our world, why does it happen at all?");
+document.querySelector(".familiesandind").innerHTML += textBubbleLightUI("Families and individuals that stock too much food that spoils before it can be sold or eaten.");
+
+document.querySelector(".toofindout").innerHTML += textBubbleLightUI("To find out how much good food Canadians throw away");
+document.querySelector(".back").innerHTML += BackNextButtonUI("Back");
+document.querySelector(".next").innerHTML += BackNextButtonUI("Next");
+
 // document.querySelector().innerHTML += TabUI("Hello");
 
 // document.querySelector().innerHTML += TabUI();
