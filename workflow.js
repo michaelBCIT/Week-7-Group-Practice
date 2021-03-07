0cce41884c289b56a7e2a152bff914260474e575
ariellecastro <caastrox@gmail.com>
2021-03-07 12:32:46 -0800

    edited page 8

--- workflow.js
+++ workflow.js
@@ -72,9 +72,9 @@ document.querySelector(".textbubble7-1").innerHTML += textBubbleLightUI("That's 
 
 //page 8 
 document.querySelector(".headerpage8").innerHTML += HeaderUI("HOW YOU CAN REDUCE FOOD WASTE");
-document.querySelector(".tip1").innerHTML += MyButtonUI("TIP 1", "./stories/info-pics/");
-document.querySelector(".tip1").innerHTML += MyButtonUI("TIP 2");
-document.querySelector(".tip1").innerHTML += MyButtonUI("TIP 3");
+document.querySelector(".tip1").innerHTML += MyButtonUI("TIP 1", "../../stories/info-pics/clean_table_icons/bread.png");
+document.querySelector(".tip2").innerHTML += MyButtonUI("TIP 2", "../../stories/info-pics/clean_table_icons/frying-pan.png");
+document.querySelector(".tip3").innerHTML += MyButtonUI("TIP 3", "../../stories/info-pics/clean_table_icons/check-list.png");
 document.querySelector(".BackButton-Page8-1").innerHTML += BackNextButtonUI("Back");
 document.querySelector(".NextButton-Page8-2").innerHTML += BackNextButtonUI("Next");
 
