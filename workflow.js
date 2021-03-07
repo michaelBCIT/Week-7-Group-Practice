3cbb6f58157960c46ee2c3961bff7acf0a5f0a27
Michael <mtrinh8@my.bcit.ca>
2021-03-06 17:51:09 -0800

    push page 3

--- workflow.js
+++ workflow.js
@@ -10,10 +10,17 @@ document.querySelector(".button-p2-2").innerHTML += BackNextButtonUI("Next");
 
 //page 3
 document.querySelector(".headerpage3").innerHTML += HeaderUI("THE COST OF FOOD WASTE");
+
 document.querySelector(".textbubblepage3-1").innerHTML += textBubbleLightUI("Food waste negatively contributes to resource costs.", "245px", "#F5D9B7");
-document.querySelector(".textbubblepage3-2").innerHTML += textBubbleLightUI("RESOURCE COST", "204px", "#FBAD81", "18px", "white");
-document.querySelector(".textbubblepage3-3").innerHTML += textBubbleLightUI("Large amounts of money is used to farm resources and crops. If thrown away, all the money that went into into those crops are wasted as well", "290px", "#F5D9B7", "18px");
+
+document.querySelector(".subheaderpage3-1").innerHTML += SubHeaderUI("40px", "204px", "16pt", "RESOURCE COST");
+
+document.querySelector(".textbubblepage3-2").innerHTML += textBubbleLightUI("Large amounts of money are used to farm resources and crops. If thrown into the waste, all the money that went into those crops are wasted too.", "291px", "#F5D9B7");
+
+document.querySelector(".subheaderpage3-2").innerHTML += SubHeaderUI("63px", "361px", "24px", "ENVIRONMENTAL COSTS");
+
 document.querySelector(".button-p3-1").innerHTML += BackNextButtonUI("Back");
+
 document.querySelector(".button-p3-2").innerHTML += BackNextButtonUI("Next");
 
 
