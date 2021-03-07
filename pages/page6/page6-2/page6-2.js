e9067b27cb9c23c4709ff9b7a10e6e83657bce4d
AliciaYien <ayien1@my.bcit.ca>
2021-03-07 12:50:30 -0800

    page6-2

--- pages/page6/page6-2/page6-2.js
+++ pages/page6/page6-2/page6-2.js
@@ -3,5 +3,5 @@ document.querySelector(".header6-2").innerHTML += HeaderUI("HOW MUCH FOOD DO CAN
 
 document.querySelector(".percent").innerHTML += textBubbleLightUI("63% of the food Canadians throw away could have been eaten.", "200px", "#FE9179", "18px", "#FFFFFF");
 
-document.querySelector(".backnext").innerHTML += BackNextButtonUI("Back");
-document.querySelector(".backnext").innerHTML += BackNextButtonUI("Next");
+document.querySelector(".back").innerHTML += BackNextButtonUI("Back");
+document.querySelector(".next").innerHTML += BackNextButtonUI("Next");
