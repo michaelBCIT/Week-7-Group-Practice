2bdab19ede9929d7dcd790ce318a3a5ead70f42d
Michael <mtrinh8@my.bcit.ca>
2021-03-07 18:10:12 -0800

    buttonanimation gang updates

--- comps/ButtonAnimation/index.js
+++ comps/ButtonAnimation/index.js
@@ -1,6 +1,8 @@
 function MyButtonAnimationUI(text="TIP 1", src="https://www.flaticon.com/svg/vstatic/svg/135/135763.svg?token=exp=1614648432~hmac=9e642e1bafc99f9244e976aa3c4fbdf9", w="125px"){
   return `
-    <div style='
+    <div 
+    class="tipsButton" 
+    style='
     height: 130px;
     width: ${w};
     border-radius: 10px;
@@ -12,20 +14,17 @@ function MyButtonAnimationUI(text="TIP 1", src="https://www.flaticon.com/svg/vst
     -webkit-box-shadow: 0 10px 6px -6px #777;
     -moz-box-shadow: 0 10px 6px -6px #777;
          box-shadow: 0 10px 6px -6px #777;
+    '
+    onclick="clickedTip(this)";
+    >
 
-    transition-property: width;
-    transition-duration: 0.5s;
-    transition-timing-function: linear;
-    onclick="clickedTip()"
-    '>
-
-    <div style='
-      padding-top: 10px;
-      align-items: center;
-      justify-content: center;
-      display: flex;
-    '>
-    ${text}</div>
+        <div style='
+        padding-top: 10px;
+        align-items: center;
+        justify-content: center;
+        display: flex;
+        '>
+        ${text}</div>
 
       <img src= ${src} alt="bread" 
         style= '
@@ -38,10 +37,21 @@ function MyButtonAnimationUI(text="TIP 1", src="https://www.flaticon.com/svg/vst
         padding-left: 20px;
         '>
 
+        <div style=" 
+        padding-top: 10px;
+        align-items: center;
+        justify-content: center;
+        display: flex;
+        "
     </div>
 
   `
 
+}
+
+function clickedTip(el){
+    document.querySelector(".tipsButton").style.width = "125px";
+    el.style.animation = "expandAnim 0.4s linear 0s 1 forwards";
 }
 
 //export const ButtonUI = MyButtonUI();
--- pages/page8/page8-2/page8-2.js
+++ pages/page8/page8-2/page8-2.js
@@ -1,7 +1,7 @@
 document.querySelector(".headerpage8-2").innerHTML += HeaderUI("HOW YOU CAN REDUCE FOOD WASTE");
 document.querySelector(".tip1-2").innerHTML += MyButtonAnimationUI("TIP 1", "../../../stories/info-pics/clean_table_icons/bread.png");
-document.querySelector(".tip2-2").innerHTML += MyButtonUI("TIP 2", "../../../stories/info-pics/clean_table_icons/frying-pan.png");
-document.querySelector(".tip3-2").innerHTML += MyButtonUI("TIP 3", "../../../stories/info-pics/clean_table_icons/check-list.png");
+document.querySelector(".tip2-2").innerHTML += MyButtonAnimationUI("TIP 2", "../../../stories/info-pics/clean_table_icons/frying-pan.png");
+document.querySelector(".tip3-2").innerHTML += MyButtonAnimationUI("TIP 3", "../../../stories/info-pics/clean_table_icons/check-list.png");
 document.querySelector(".BackButton-Page8-1-2").innerHTML += BackNextButtonUI("Back");
 document.querySelector(".NextButton-Page8-2-2").innerHTML += BackNextButtonUI("Next");
 
